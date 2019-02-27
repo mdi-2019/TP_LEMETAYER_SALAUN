5e027c406d70b88fec2d1e18fcfb7e119a5076e1
Totorro35 <thomas.lemetayer.35@gmail.com>
2019-02-27 11:29:32 +0100

    [CHANGE] <Un début à tout>
    Il etait temps que notre calculatrice voit le jour
    Nous avons donc commence lui donner une âme.

--- src/main/java/org/stmv/project_maven/App.java
+++ src/main/java/org/stmv/project_maven/App.java
@@ -1,5 +1,7 @@
 package org.stmv.project_maven;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "\n_____________________________________________________\n" );
+        System.out.println( "	Bienvenue dans la super calculatrice" );
+        System.out.println( "\n_____________________________________________________\n" );
+        Scanner sc = new Scanner(System.in);
+        
+        boolean exit=false;
+        while(!exit) {
+        	System.out.println("Veuillez saisir un mot :");
+        	String str = sc.nextLine();
+        	System.out.println("Vous avez saisi : " + str);
+        	if(str.equals("exit")) {
+        		exit=true;
+        	}
+        }
+        
+        System.out.println( "\n_____________________________________________________\n" );
+        System.out.println( "	Merci!!!!!!!!!!" );
+        System.out.println( "\n_____________________________________________________\n" );
     }
 }
