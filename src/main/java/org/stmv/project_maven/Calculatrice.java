a2afb7ee5a39558207e05f75ccf3b78971128fbb
Salaun Corentin <corentin.salaun@etudiant.univ-rennes1.fr>
2019-02-27 11:56:36 +0100

    Add fonction less

---
+++ src/main/java/org/stmv/project_maven/Calculatrice.java
@@ -0,0 +1,15 @@
+package org.stmv.project_maven;
+
+public class Calculatrice {
+
+	
+	public double add(double nombre1, double nombre2)
+	{
+		return nombre1 + nombre2;
+	}
+	
+	public double less(double nombre1, double nombre2)
+	{
+		return nombre1 - nombre2;
+	}
+}
