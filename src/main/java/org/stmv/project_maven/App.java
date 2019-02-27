a3a879d6527483713fc62619f8db64485caaef6b
Totorro35 <thomas.lemetayer.35@gmail.com>
2019-02-27 11:17:05 +0100

    [CHANGE] <Initial commit> Initialisation du projet
    This commit does the following:

---
+++ src/main/java/org/stmv/project_maven/App.java
@@ -0,0 +1,13 @@
+package org.stmv.project_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
